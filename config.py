6bc6f0c39f7b3d95a9c53dbb47aad543d67a15a9
Can Kabuloglu <ckabuloglu@gmail.com>
2018-02-27 15:42:29 -0500

    db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost/fresh"
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/fresh"
