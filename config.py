a0f34064759181576608506d79a6524ea037c2f4
Can Kabuloglu <ckabuloglu@gmail.com>
2018-02-27 18:46:00 -0500

    db

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 DEBUG = False
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-# SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/fresh"
+SQLALCHEMY_DATABASE_URI = "postgresql://ckabuloglu@gmail.com:YWt-NZH-Hz7-Znw@localhost:5432/fresh"
+# SQLALCHEMY_DATABASE_URI = "postgresql://localhost/fresh"
--- fresh/database.py
+++ fresh/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from . import app
-import os
+
+print app.config['SQLALCHEMY_DATABASE_URI']
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
